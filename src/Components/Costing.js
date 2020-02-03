2b8a81d6ed4d224be431a8c0f7377f7a184f3d31
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-02-03 18:55:44 +0530

    second commit

--- src/Components/Costing.js
+++ src/Components/Costing.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import PricingCard from './CardforPricing';
 
 const Costing = () => {
     return (
-        <div>
-            I am from the costing
+        <div style={{display:"grid", gridTemplateColumns:"3fr 3fr 3fr", gridGap:"10px"}}>
+            <PricingCard bhk="3 BHK + 2 T"/>
+            <PricingCard bhk="3 BHK + 3 T"/>
+            <PricingCard bhk="4 BHK + 4 T"/>
+            
         </div>
     );
 };
