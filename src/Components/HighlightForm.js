be1f76ad3ab04fab81d6f391b0a6a5ced67555f9
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-02-07 18:37:23 +0530

    last commit

--- src/Components/HighlightForm.js
+++ src/Components/HighlightForm.js
@@ -4,21 +4,21 @@ import '../GlobalStyles/styles.css';
 const HighlightForm = () => {
     return (
         <div>
-        <form id="demo-form">
+        <form>
         <div className="quote-form">
                 <div className="form-head">
                 <h1>ENQUIRE NOW</h1>
                 </div>
                 <div className="form-group">
-                    <input type="text" name="name" value="" placeholder="Your Name" required=""/>
+                    <input type="text" name="name" placeholder="Your Name" required/>
                 </div>
                             
             <div className="form-group">
-                <input type="email" name="email" value="" placeholder="Your Email" required=""/>
+                <input type="email" name="email" placeholder="Your Email" required/>
             </div>
                             
             <div className="form-group">
-                <input type="text" name="phone_no" value="" placeholder="Your Phone" minlength="10" maxlength="20" title="Must have 10 digit numbers" pattern="^\d{10}$" required=""/>
+                <input type="text" name="phone_no" placeholder="Your Phone" minlength="10" maxlength="20" title="Must have 10 digit numbers" pattern="^\d{10}$" required/>
             </div>
                                         
                 <div className="form-button">
