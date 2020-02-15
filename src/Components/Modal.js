2c9e148a685260bcdfc1c50f762deb57addde6b9
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-02-15 18:16:15 +0530

    third commit

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -18,7 +18,7 @@ const Modal = (props) => {
           <Form/>
           <br/>
           <br/>
-          <div style={{width: "100%", background: "linear-gradient(101deg, rgb(69, 190, 61), #056C9F)", color:"white", textAlign: "center", padding:"10px 0px"}}>
+          <div style={{width: "100%", background: "linear-gradient(101deg, rgb(69, 190, 61), #ff9800)", color:"white", textAlign: "center", padding:"10px 0px"}}>
           <a href="tel:01234567890"><img src={Callusback} style={{maringLeft:"10px"}}/>  <span style={{color:"white"}}>9071354854</span></a> 
           </div>
         </Modalabc>
