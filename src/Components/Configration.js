f6281d13107a7a8a9f33ef9e68fac1cc1d7efba4
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-02-15 13:59:16 +0530

    second push

--- src/Components/Configration.js
+++ src/Components/Configration.js
@@ -16,18 +16,18 @@ const Tabel = () => {
             <img src={Prelaunch}/>
              <div>
               <span><h1>2 BHK 2T -</h1> <h2>1141 SQ FT</h2></span>
-              <span> <h2>Rs. 84.57 L</h2></span>
-              <span> <h2>Rs. 79.18 L</h2></span>
+              <span> <h3>Rs. 84.57 L</h3></span>
+              <span> <h4>Rs. 79.18 L</h4></span>
              </div>
              <div>
              <span><h1>2 BHK 2T -</h1>  <h2>1141 SQ FT</h2></span>
-             <span><h2> Rs. 84.57 L</h2></span>
-               <span><h2> Rs. 84.57 L</h2></span>
+             <span><h3> Rs. 84.57 L</h3></span>
+               <span><h4> Rs. 84.57 L</h4></span>
             </div>
             <div>
             <span><h1>2 BHK 2T -</h1> <h2>1141 SQ FT</h2></span>
-            <span><h2> Rs. 84.57 L</h2></span>
-              <span><h2> Rs. 84.57 L</h2></span>
+            <span><h3> Rs. 84.57 L</h3></span>
+              <span><h4> Rs. 84.57 L</h4></span>
            </div>
             </div>
             </div>
@@ -45,22 +45,34 @@ const tabel = css`
     border:2px solid black;
   }
   div{
-    width: 880px;
-    margin-left: 17%;
+    width: 900px;
+    margin-left: 18%;
     display:flex;
     span{
-      padding:20px 70px;
+      padding: 20px 85px 0px 67px;
       border-bottom:0.02px solid #eee;
       display:flex;
       h1{
         font-weight:bold;
         font-size:1.5em;
       }
+      h3{
+        text-decoration: line-through;
+        font-size:1.1em;
+        margin-top:6px;
+        text-decoration-color:orange;
+      }
       h2{
         font-weight:450;
         font-size:1.2em;
         margin-top:5px;
-        margin-left:4px;
+       
+      }
+      h4{
+        font-weight:480;
+        font-size:1.3em;
+        margin-top:4px;
+       
       }
     }
   }
