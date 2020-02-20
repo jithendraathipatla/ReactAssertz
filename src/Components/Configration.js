abc2c6365ae847976c7ca45eb7a366d39c1a8282
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-02-20 14:31:58 +0530

    second commit

--- src/Components/Configration.js
+++ src/Components/Configration.js
@@ -94,6 +94,12 @@ const tabel = css`
           h2{
             font-size:0.8em;
           }
+          h3{
+            font-size:0.9em;
+          }
+          h4{
+            font-size:1em;
+          }
           :nth-child(1){
             display:flex;
           }
