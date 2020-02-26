82b8c3ab51bbbb996e2aaac8fcbd625d6c263fa6
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-02-26 16:02:18 +0530

    third commit

--- src/Components/New_modal.js
+++ src/Components/New_modal.js
@@ -1,19 +1,20 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Modalabc from 'react-responsive-modal';
 import Form from './Form';
 import '../GlobalStyles/styles.css';
 import OfficialLogo from '../Imagesa/Images/logo.png';
 import Callusback from '../Imagesa/Images/svg/call-back.svg'
 
-const Modal = (props) => {
+const Modalabcd = (props) => {
     const [isopen, setisopen] = useState(false);
+
       return (
         <div className="finaltouch">
         <a onClick={() => setisopen(true)} className={props.class}>{props.title}</a>
         <Modalabc open={isopen} onClose={ () => setisopen(false)}>
        
         <div style={{textAlign:"center"}}>
-        <img className="modalImageLogo" src={OfficialLogo} alt="Marq 2.0"/>
+        <img className="modalImageLogo" src={OfficialLogo} alt="Waterford"/>
         </div>
           <Form/>
           <br/>
@@ -26,4 +27,4 @@ const Modal = (props) => {
     );
 };
     
-export default Modal;
+export default Modalabcd;
