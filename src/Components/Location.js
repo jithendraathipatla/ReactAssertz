f103b00fc74b9fbce5374478d7b11cafe04c1203
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-03-13 08:00:46 +0530

    second commit

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -11,7 +11,7 @@ const Location = () => {
         <div style={{textAlign:"center"}}>
         <Title title="FinsBury Park Location"/>
         </div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15541.291396491108!2d77.684435!3d13.142024!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd0be65d7c84b08a7!2sPrestige%20Finsbury%20Park!5e0!3m2!1sen!2sin!4v1577365700493!5m2!1sen!2sin" width="100%" height="517" frameborder="0" style={{border:"0"}} allowfullscreen=""></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.093988886768!2d77.72165691482259!3d13.029686390817787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1037f4ecfb4b%3A0xe7e06043fc59c9f2!2sConcorde%20Auriga!5e0!3m2!1sen!2sin!4v1584065284113!5m2!1sen!2sin" width="100%" height="517" frameborder="0" style={{border:"0"}} allowfullscreen=""></iframe>
         </div>
         <div>
         <div style={{textAlign:"center"}}>
--- src/Components/SEO.js
+++ src/Components/SEO.js
@@ -8,7 +8,6 @@ const SiteMetadata = ({ pathname, locale, title, des }) => {
 
 
   return (
-    console.log(des, title),
     <Helmet defer={false} defaultTitle={title} titleTemplate={`%s | ${title}`}>
       <html lang={locale} />
       <meta name="docsearch:version" content="2.0" />
@@ -22,7 +21,6 @@ const SiteMetadata = ({ pathname, locale, title, des }) => {
       <meta property="og:type" content="website" />
       <meta property="og:locale" content={locale} />
       <meta property="og:site_name" content={des} />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"/>
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"/>      
     </Helmet>
   )
