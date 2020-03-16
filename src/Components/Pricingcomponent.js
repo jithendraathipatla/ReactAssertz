4339ab55f9f15b126c0b43cb2534d494c93e55f9
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-03-16 17:37:32 +0530

    second commit

--- src/Components/Pricingcomponent.js
+++ src/Components/Pricingcomponent.js
@@ -103,11 +103,9 @@ const Pricingcomponent = (props) => {
   border:1px solid blue;
   background:blue;
   color:white;
-  padding:10px;
-  position:absloute;
-  div{
-      margin:10px;
-  }
+  background:linear-gradient(-90deg,#d3b169,#000,#d3b169,#000);
+    background-size: 400% 400%;
+    box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
 `
 
  export default Pricingcomponent;
