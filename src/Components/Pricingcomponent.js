45ddb1b5d3b28311c08ffd9b79b00d5956ca72ba
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-03-16 18:35:00 +0530

    third commit

--- src/Components/Pricingcomponent.js
+++ src/Components/Pricingcomponent.js
@@ -106,12 +106,9 @@ const Pricingcomponent = (props) => {
   background:linear-gradient(-90deg,#d3b169,#000,#d3b169,#000);
     background-size: 400% 400%;
     box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
-    a{
         :hover{
-        color:white;
-        transform:translateY(-0.1px);
+        color:white !important;
         }
-    }
 `
 
  export default Pricingcomponent;
