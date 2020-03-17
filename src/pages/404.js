558a309029eb4fdf6c764b0f9b9956ecd2fb5c88
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-03-17 17:28:25 +0530

    second commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import {Link} from 'gatsby'
-import Layout from '../Components/Layout';
+import {Link} from 'gatsby';
+import {css} from '@emotion/core'
+import Navbar from '../Components/NavBar';
 
 const ErrorPage = () => {
     return (
-        <Layout title="About Prestige group | Prestige Properties ">
-        <button><Link to="/">Home</Link></button>
-        </Layout>
+        <div>
+          <Navbar/>
+          <div>
+           <button css={button}><Link to="/">Home</Link></button>
+          </div>
+        </div>
     );
 };
+
+const button = css`
+    margin: 80vh 225vh;
+    padding: 9px;
+    border-radius: 4px;
+    border: 1px solid #eeee;
+    box-shadow: 0px 0px 6px 1px;
+
+`
 
 export default ErrorPage;
