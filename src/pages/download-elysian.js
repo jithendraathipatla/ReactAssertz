ef343972a5a54cb2fe80879f36332808958bd366
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-03-17 16:57:16 +0530

    second commit

--- src/pages/download-elysian.js
+++ src/pages/download-elysian.js
@@ -6,7 +6,7 @@ const download = () => {
         <div style={{textAlign:"center"}}>
         <SEOComponent title="Prestige Elysian"/>
          <h4>Sent Successfully! Thank you, We will contact you shortly!</h4>
-         <h4><a href="https://drive.google.com/open?id=1gD1dqRdfmSLXclBQSC43a3VBY3CNOj84" target="_blank">Elysian Brochure</a></h4>
+         <h4><a href="https://drive.google.com/file/d/1uU5f8_sV5u4-YCPYui3M_2XcA8KwpBzY/view?usp=sharing" target="_blank">Elysian Brochure</a></h4>
 	    </div>
     );
 };
--- src/pages/download-kew_gardens.js
+++ src/pages/download-kew_gardens.js
@@ -6,7 +6,7 @@ const download = () => {
         <div style={{textAlign:"center"}}>
         <SEOComponent title="Prestige Kew Gardens"/>
         <h4>Sent Successfully! Thank you, We will contact you shortly!</h4>
-        <h4><a href="https://drive.google.com/open?id=1S6RsDbGVtaIDhW5EY5jI-ZTsesoBjQkN" target="_blank">Kew Gardens borchure</a></h4>
+        <h4><a href="https://drive.google.com/file/d/1S6RsDbGVtaIDhW5EY5jI-ZTsesoBjQkN/view?usp=sharing" target="_blank">Kew Gardens borchure</a></h4>
 	    </div>
     );
 };
