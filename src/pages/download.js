147bc6e7252cf027cf2def903782ee2c441b2296
jithendraathipatla <37688287+jithendraathipatla@users.noreply.github.com>
2020-08-19 15:15:30 +0530

    second commit

--- src/pages/download.js
+++ src/pages/download.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SEOComponent from '../Components/SEO';
+import SEOComponent from '../components/seo';
 
 const download = () => {
     return (
@@ -7,8 +7,9 @@ const download = () => {
         <div style={{textAlign:"center"}}>
         <SEOComponent title="Prestige Waterford Apartments"/>
 	     <h1>Sent Successfully! Thank you, We will contact you shortly!</h1>
-		<h3><a href="https://drive.google.com/open?id=1cQjo-FwWNmCqFv_71lLac2heQyOIGTZT">Click here to download Brochure</a></h3>
-	        </div>
+		<h3><a href="https://drive.google.com/file/d/17-0h034r_dVfOg2HP52DY65SbmpZBIbN/view?usp=drivesdk" target="_blank">Click here to download Brochure</a></h3>
+        <h3>We will soon update the Cost Sheet</h3>
+	    </div>
     );
 };
 
